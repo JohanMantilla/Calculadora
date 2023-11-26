bad3866903316a81b4c33f858ad24e0661f9c117
JohanMantilla <johan.mantilla01@epn.edu.ec>
2023-11-26 02:05:55 -0500

    Adding message change on CalculatorExecute class.

--- src/main/java/ec/edu/epn/git/calculator/CalculatorExecute.java
+++ src/main/java/ec/edu/epn/git/calculator/CalculatorExecute.java
@@ -3,18 +3,19 @@ package ec.edu.epn.git.calculator;
 
 public class CalculatorExecute {
     public static void main(String[] args) {
+        System.out.println("!! Change !!");
+        System.out.println("*----- ESCUELA POLITÉCNICA NACIONAL ---*");
         System.out.println("== Calculator Execute ==");
 
         Calculator c = new Calculator();
         int addition = c.addition(4, 7);
         System.out.println("c.addition(4,7) = " + addition);
 
-
         int subtraction = c.subtraction(7, 1);
         System.out.println("c.subtraction(7,1) = " + subtraction);
-
+        System.out.println("!! Change !!");
+        System.out.println("INTEGRANTES GR4: Erick Cabezas, Johan Mantilla, Sebastian Moyano, Christopher Zambrano");
     }
-
 
 }
 
